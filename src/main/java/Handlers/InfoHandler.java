65df5bba0ffbb7428863643bf89d4ebc60945177
Auramaster13 <thelegotechhead@yahoo.com.au>
2016-11-23 13:49:34 +1100

    Fixed UpdateInfo
    Changed how the messages were sent
    allowed for #split# tag which tells the bot to split the
    #info message dumps then and there.

--- src/main/java/Handlers/InfoHandler.java
+++ src/main/java/Handlers/InfoHandler.java
@@ -34,10 +34,11 @@ public class InfoHandler {
         String lastChunk;
         String nextChunk;
         String imageTag;
-        String[] imageSplit;
+        String[] splited;
         String lastAttepmt;
         String imagePrefix = "#image#{";
         String imageSuffix = "}";
+        String tagBreak = "#split#";
         String image;
 
         String[] contentsSplit;
@@ -46,12 +47,20 @@ public class InfoHandler {
             //this ignores commented out code.
             if (!s.startsWith("//")){
                 if (builder.toString().contains(imagePrefix)){
-                    imageTag = StringUtils.substringBetween(builder.toString(),imagePrefix,imageSuffix);
-                    imageSplit = builder.toString().split(imageTag);
-                    lastChunk = imageSplit[0];
-                    nextChunk = imageSplit[1];
+                    imageTag = imagePrefix + StringUtils.substringBetween(builder.toString(),imagePrefix,imageSuffix) + imageSuffix;
+                    splited = builder.toString().split(Pattern.quote(imageTag));
+                    lastChunk = splited[0];
+                    nextChunk = splited[1];
                     stringChunks.add(lastChunk);
                     stringChunks.add(imageTag);
+                    builder.delete(0,builder.length());
+                    builder.append(nextChunk);
+                }
+                if (builder.toString().contains(tagBreak)){
+                    splited = builder.toString().split(Pattern.quote(tagBreak));
+                    lastChunk = splited[0];
+                    nextChunk = splited[1];
+                    stringChunks.add(lastChunk);
                     builder.delete(0,builder.length());
                     builder.append(nextChunk);
                 }
@@ -61,61 +70,77 @@ public class InfoHandler {
                 }
                 builder.append(doTextTags(s) + "\n");
             }
-            stringChunks.add(builder.toString());
-            builder.delete(0, builder.length());
         }
+        stringChunks.add(builder.toString());
+        builder.delete(0, builder.length());
 
 
         //actual code.
-        for (String contents : stringChunks) {
+        for (String contents : stringChunks){
+            contents = TagSystem.tagNoNL(contents);
+            contents = TagSystem.tagEmoji(contents,guild);
+            if (contents.contains(imagePrefix)){
+                image = StringUtils.substringBetween(contents, imagePrefix, imageSuffix);
+                File file = new File(Utility.getGuildImageDir(guild.getID()) + image);
+                Utility.sendFile("",channel,file);
+            }else {
+                Utility.sendMessage(contents,channel);
+            }
             try {
-                contents = TagSystem.tagNoNL(contents);
-                contents = TagSystem.tagEmoji(contents,guild);
-                //test to see if adding the next set will make the message to long and stops that.
-                if ((builder.toString() + contents).length() > 1800) {
-                    Utility.sendMessage(builder.toString(), channel);
-                    Thread.sleep(2000);
-                    builder.delete(0, builder.length());
-                }
-                do {
-                    lastAttepmt = contents;
-                    String toAppend = "";
-                    if (contents.contains(imagePrefix)) {
-                        //getting image file location
-                        image = StringUtils.substringBetween(contents, imagePrefix, imageSuffix);
-                        //splitting it so that the stuff before the image can be sent
-                        contentsSplit = contents.split(Pattern.quote(imagePrefix + image + imageSuffix));
-
-                        //if the first part of the array isn't empty get that.
-                        if (contentsSplit.length != 0) {
-                            toAppend = contentsSplit[0];
-                        }
-                        //append that to the thinger
-                        builder.append(toAppend);
-                        //send what you have already and then clear the builder.
-                        Utility.sendMessage(builder.toString(), channel);
-                        builder.delete(0, builder.length());
-                        Thread.sleep(2000);
-                        //clear the stuff that was just sent and the the image code from things
-                        contents = contents.replaceFirst(Pattern.quote(toAppend + imagePrefix + image + imageSuffix), "");
-                        //set up the file
-                        File file = new File(Utility.getGuildImageDir(guild.getID()) + image);
-                        //if it exists send it if not send and error
-                        if (!file.exists()) {
-                            Utility.sendMessage(file.getPath() + " Does not Exist", channel);
-                            Thread.sleep(2000);
-                        } else {
-                            Utility.sendFile("", channel, file);
-                        }
-                    }
-                } while (!contents.equals(lastAttepmt) && contents.contains(imagePrefix));
-                builder.append(contents);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        //after everything send the last set
-        Utility.sendMessage(builder.toString(), channel);
+//        for (String contents : stringChunks) {
+//            try {
+//                contents = TagSystem.tagNoNL(contents);
+//                contents = TagSystem.tagEmoji(contents,guild);
+//                //test to see if adding the next set will make the message to long and stops that.
+//                if ((builder.toString() + contents).length() > 1800) {
+//                    Utility.sendMessage(builder.toString(), channel);
+//                    Thread.sleep(2000);
+//                    builder.delete(0, builder.length());
+//                }
+//                do {
+//                    lastAttepmt = contents;
+//                    String toAppend = "";
+//                    if (contents.contains(imagePrefix)) {
+//                        //getting image file location
+//                        image = StringUtils.substringBetween(contents, imagePrefix, imageSuffix);
+//                        //splitting it so that the stuff before the image can be sent
+//                        contentsSplit = contents.split(Pattern.quote(imagePrefix + image + imageSuffix));
+//
+//                        //if the first part of the array isn't empty get that.
+//                        if (contentsSplit.length != 0) {
+//                            toAppend = contentsSplit[0];
+//                        }
+//                        //append that to the thinger
+//                        builder.append(toAppend);
+//                        //send what you have already and then clear the builder.
+//                        Utility.sendMessage(builder.toString(), channel);
+//                        builder.delete(0, builder.length());
+//                        Thread.sleep(2000);
+//                        //clear the stuff that was just sent and the the image code from things
+//                        contents = contents.replaceFirst(Pattern.quote(toAppend + imagePrefix + image + imageSuffix), "");
+//                        //set up the file
+//                        File file = new File(Utility.getGuildImageDir(guild.getID()) + image);
+//                        //if it exists send it if not send and error
+//                        if (!file.exists()) {
+//                            Utility.sendMessage(file.getPath() + " Does not Exist", channel);
+//                            Thread.sleep(2000);
+//                        } else {
+//                            Utility.sendFile("", channel, file);
+//                        }
+//                    }
+//                } while (!contents.equals(lastAttepmt) && contents.contains(imagePrefix));
+//                builder.append(contents);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        //after everything send the last set
+//        Utility.sendMessage(builder.toString(), channel);
     }
 
 
