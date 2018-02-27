f6808b3384baa0dd5e63429f399ec3d32559c2dd
Andriel Chaoti <accounts@vandesdelca32.net>
2018-02-26 20:58:55 -0700

    Add new build .jar

--- src/main/java/com/github/vaerys/commands/admin/Test.java
+++ src/main/java/com/github/vaerys/commands/admin/Test.java
@@ -3,10 +3,10 @@ package com.github.vaerys.commands.admin;
 import com.github.vaerys.commands.CommandObject;
 import com.github.vaerys.enums.ChannelSetting;
 import com.github.vaerys.enums.SAILType;
+import com.github.vaerys.handlers.setupStages.SettingsStage;
 import com.github.vaerys.objects.SubCommandObject;
 import com.github.vaerys.templates.Command;
 import sx.blah.discord.handle.obj.Permissions;
-import sx.blah.discord.util.DiscordException;
 
 /**
  * Created by Vaerys on 30/01/2017.
@@ -14,7 +14,7 @@ import sx.blah.discord.util.DiscordException;
 public class Test extends Command {
 
     protected static final SubCommandObject object = new SubCommandObject(
-            new String[]{"Boop","Snugg"},
+            new String[]{"Boop", "Snugg"},
             "[Test Data]",
             "nothing",
             SAILType.ADMIN
@@ -23,28 +23,8 @@ public class Test extends Command {
 
     @Override
     public String execute(String args, CommandObject command) {
-//
-//        IEmoji emoji = command.guild.getEmojiByName(args);
-//        if (emoji == null) return "> Not a valid emoji name.";
-//        return emoji.toString();
-
-//        XEmbedBuilder builder = new XEmbedBuilder(command);
-//        builder.withAuthorName("Note 1 - " +command.user.displayName);
-//        builder.withAuthorIcon(command.user.getAvatarURL());
-//        builder.withDescription("blah blah this is a note.\n\n" +
-//                "`Last edited: " + Utility.formatTime(10, true) + " ago.`");
-//        builder.withTimestamp(command.message.getTimestamp());
-//        builder.withFooterText("Created by " + command.client.bot.displayName);
-//        builder.withFooterIcon(command.client.bot.getAvatarURL());
-//        builder.send(command.channel);
-//        return nothing;
-//        return (long) ((90 - 7) * (Globals.avgMessagesPerDay * command.guild.config.xpRate * command.guild.config.xpModifier) / 8) + "";
-
-//        EnumSet<Permissions> botPerms = command.client.bot.getPermissions(command.guild);
-//        return botPerms.contains(Permissions.MANAGE_CHANNELS) ? "> I HAVE MANAGE_CHANNELS" : "> I DO NOT HAVE MANAGE_CHANNELS";
-
-        return object.isSubCommand(command) + " " + object.getArgs(command) + " " + object.getCommandUsage(command);
-//        throw new DiscordException("TestException");
+        new SettingsStage().stepText(command);
+        return null;
     }
 
     @Override
